c556009d6c002b71b52feb5e6e3ea3b8d54f8ef9
Greimer29 <greimerabelguzman29@gmail.com>
2020-12-12 21:21:44 -0400

    cambios al codice, expandir y busqueda en la DB

--- login/codice/codice.js
+++ login/codice/codice.js
@@ -1,24 +1,59 @@
-var persona = {
+//simulacion de una base de datos.
+var persona = [{
     usuario: "GreimerKmon",
-    contraseña: "2808600328086003",
+    contraseña: "28086003",
     Nombres: "Greimer Abel",
     Apellidos: "Pérez Guzmán"
-}
+}, {
+    usuario: "Kenyer",
+    contraseña: "28086005",
+    Nombres: "Kenyer Josué",
+    Apellidos: "Pérez Guzmán"
+}, {
+    usuario: "Yelito",
+    contraseña: "12652491",
+    Nombres: "Lennys Yelitza",
+    Apellidos: "Guzmán de Pérez"
+}, {
+    usuario: "Tobias",
+    contraseña: "10934709",
+    Nombres: "José Tobias",
+    Apellidos: "Pérez Martinez"
+}];
 
+//capturamos en variables los objetos de HTML
 var user = document.getElementById("usuario-caja");
 var password = document.getElementById("contraseña-caja");
 var boton = document.getElementById("boton-boton");
 var mensaje = document.getElementById("mensaje");
 
+//evento click al boton para ingresar
 boton.addEventListener("click", comparar);
 
 function comparar() {
+    if ((user.value == "" || password.value == "")) {
+        mensaje.innerHTML = "Please fill in the blank.";
+        console.log("Negado");
+    } else {
+        mensaje.innerHTML = "Sorry, but you dont exist in our Databases.";
+        console.log("Negado");
+    }
+    let getUsuarioDB = persona.find(estudiante => estudiante.contraseña === password.value && estudiante.usuario === user.value);
+    if ((getUsuarioDB)) {
+        console.log("Entraste");
+        mensaje.innerHTML = "";
+        window.location.assign('http://127.0.0.1:5500/pages/user.html');
+    }
+}
+
+/*function comparar() {
     if ((user.value == persona.usuario) && (password.value == persona.contraseña)) {
         console.log("Entraste.");
         mensaje.innerHTML = "";
+        window.location.assign('http://127.0.0.1:5500/pages/user.html');
     } else if ((user.value == "" || password.value == "")) {
         mensaje.innerHTML = "Please fill in the blank.";
     } else {
         mensaje.innerHTML = "Sorry, but you dont exist in our Databases.";
     }
-}
+}*/
