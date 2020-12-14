506f7c06712c422d5d0b8523fb1816739ef43a7a
Greimer29 <greimerabelguzman29@gmail.com>
2020-12-14 10:14:32 -0400

    cambios realizados en función de: user.html

--- login/codice/codice.js
+++ login/codice/codice.js
@@ -38,22 +38,10 @@ function comparar() {
         mensaje.innerHTML = "Sorry, but you dont exist in our Databases.";
         console.log("Negado");
     }
-    let getUsuarioDB = persona.find(estudiante => estudiante.contraseña === password.value && estudiante.usuario === user.value);
+    var getUsuarioDB = persona.find(estudiante => estudiante.contraseña === password.value && estudiante.usuario === user.value);
     if ((getUsuarioDB)) {
         console.log("Entraste");
         mensaje.innerHTML = "";
         window.location.assign('http://127.0.0.1:5500/pages/user.html');
     }
 }
-
-/*function comparar() {
-    if ((user.value == persona.usuario) && (password.value == persona.contraseña)) {
-        console.log("Entraste.");
-        mensaje.innerHTML = "";
-        window.location.assign('http://127.0.0.1:5500/pages/user.html');
-    } else if ((user.value == "" || password.value == "")) {
-        mensaje.innerHTML = "Please fill in the blank.";
-    } else {
-        mensaje.innerHTML = "Sorry, but you dont exist in our Databases.";
-    }
-}*/
